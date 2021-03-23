9001effe9d15a40bb400d65435419b180dd71115
techy3000 <will.andrews98@yahoo.co.uk>
2021-03-23 14:59:33 +0000

    Add Will

---
+++ ExampleConsoleApp/Will.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleConsoleApp
+{
+    class Will
+    {
+        public string Fname { get; set; }
+        public string Sname { get; set; }
+
+        public Will(string fname, string sname)
+        {
+            Fname = fname;
+            Sname = sname;
+        }
+    }
+}
