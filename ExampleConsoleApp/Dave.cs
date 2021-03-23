1074f7a8e5c321649b292cf3b2fd41b80d4103ca
davewave17 <davewave17@gmail.com>
2021-03-23 14:58:09 +0000

    Added Dave props

---
+++ ExampleConsoleApp/Dave.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExampleConsoleApp
+{
+    class Dave
+    {
+        public string FirstName { get; set; }
+
+        public string Surname { get; set; }
+
+        public Dave(string firstName, string surname)
+        {
+            FirstName = firstName;
+            Surname = surname;
+        }
+    }
+}
